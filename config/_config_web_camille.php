c6ff3ed0d0e0e3e446f6181dc843f9af1ae4fc14
FireBlood <tristan.turki4@gmail.com>
2025-05-20 11:53:50 +0200

    Changes

--- config/_config_web_camille.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$serveur = "127.0.0.1";
-$user = "";
-$bdd_password = "";
-$BDD_name = "";
-
-?>
--- config/_config_web_tristan.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$serveur = "127.0.0.1";
-$user = "u937355202_TristanT";
-$bdd_password = "Trist3265&";
-$BDD_name = "u937355202_TristanTBDD";
-
-?>
